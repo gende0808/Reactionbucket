c325ea26a897714287b1964f31df8b41b0161768
Gregory <Gregory@gregory.nl>
2019-06-21 16:05:56 +0200

    found it

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import json
 from discord.ext import commands
+import discord
 from discord.ext.commands import has_permissions, MissingPermissions
 
 #bot token
@@ -7,7 +8,7 @@ token = "NTY0NDg5MDk5NTEyMzgxNDQw.XKondw.feVRAvJHQvImttAd8X7EBxSkfNw"
 # Creates client and sets command prefixes to ! meaning that any function name can be called with ! before it.
 client = commands.Bot(command_prefix='.')
 
-#opens the JSON file and loads it as a JSON object
+# Opens the JSON file and loads it as a JSON object
 emojifile = open('db/Emoji.json')
 emojiDict = json.load(emojifile)
 emojifile.close()
@@ -22,21 +23,21 @@ async def modify_role(ctx, emoji="", rolename=""):
                 json.dump(emojiDict, fp)
             await ctx.send(f"Added {emoji} as the reaction to be assigned {rolename} as a role")
         else:
-            await ctx.send('Please use the correct format example: .modify_role :wink: "League Of Legends"')
+            await ctx.send('Please use the correct format: [command] [emoji] ["game"] '
+                           'example: .modify_role :wink: "League Of Legends"')
 
 
 @client.event
 async def on_raw_reaction_add(raw):
     channel = client.get_channel(raw.channel_id)
-    user = client.get_user(raw.user_id)
+    user: discord.member = client.get_user(raw.user_id)
     if channel.name == 'buckettest':
         if raw.emoji in emojiDict:
             # Finds out what 'guild' a user is in and gets the role by ID
             role = user.guild.get_role(emojiDict[raw.emoji])
             await user.add_roles(role)
     if channel.name == 'roles':
-        print(json.load(raw.emoji))
-
+        print(json.dumps(raw.emoji.name))
 
 @client.event
 async def on_raw_reaction_remove(raw):
@@ -49,6 +50,6 @@ async def on_raw_reaction_remove(raw):
             await user.remove_roles(role)
     if channel.name == 'roles':
         print("Hey")
-        print(raw.emoji)
+        print(raw.emoji.name.encode('utf-8'))
 
 client.run(token)
