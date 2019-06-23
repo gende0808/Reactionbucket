be2ab87e5f6241744eaa266073cee0ed9019ee03
Gregory <gende@novacollege.nl>
2019-06-23 23:07:24 +0200

    lots of progress

--- bot.py
+++ bot.py
@@ -1,55 +1,91 @@
 import json
 from discord.ext import commands
 import discord
-from discord.ext.commands import has_permissions, MissingPermissions
+import mysql.connector
 
-#bot token
+
+connection = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="bucketbot",
+  use_unicode=True,
+  charset='utf8mb4'
+)
+
+# bot token
 token = "NTY0NDg5MDk5NTEyMzgxNDQw.XKondw.feVRAvJHQvImttAd8X7EBxSkfNw"
 # Creates client and sets command prefixes to ! meaning that any function name can be called with ! before it.
-client = commands.Bot(command_prefix='.')
+client: commands.Bot = commands.Bot(command_prefix='.')
 
 # Opens the JSON file and loads it as a JSON object
+roles_channel = 'buckettest'
 emojifile = open('db/Emoji.json')
-emojiDict = json.load(emojifile)
+emojiList = json.load(emojifile)
 emojifile.close()
+mycursor = connection.cursor()
+for records in emojiList:
+    print(records)
+    sql = f"INSERT INTO emoji (message_id, emoji_id, role_id) VALUES (%s, %s, %s)"
+    val = (records[0], records[1], records[2])
+    mycursor.execute(sql, val)
+    connection.commit()
+
+
+@client.event
+async def on_command_error(ctx, error):
+    if ctx.message.channel.name == 'buckettest':
+        await ctx.send('Please use the correct format: [command] [message ID] [emoji] ["existing role"] '
+                       'example: .manage_role 50542471247133859 :wink: "League Of Legends"')
 
 
 @client.command()
-async def modify_role(ctx, emoji="", rolename=""):
+async def manage_role(ctx, message_id: discord.Message.id, emoji, role: discord.Role):
     if ctx.author.top_role.name == "Admin":
-        if emoji != "" and rolename != "":
-            emojiDict.update({emoji: rolename})
-            with open('db/Emoji.json', 'w') as fp:
-                json.dump(emojiDict, fp)
-            await ctx.send(f"Added {emoji} as the reaction to be assigned {rolename} as a role")
-        else:
-            await ctx.send('Please use the correct format: [command] [emoji] ["game"] '
-                           'example: .modify_role :wink: "League Of Legends"')
+        if isinstance(emoji, discord.Emoji):
+            emoji = emoji.id
+        emojiList.append([message_id, emoji, role.id])
+        with open('db/Emoji.json', 'w') as fp:
+            json.dump(emojiList, fp)
+        await ctx.send(f"Added {emoji} as the reaction to be assigned {role} as a role")
 
 
 @client.event
 async def on_raw_reaction_add(raw):
     channel = client.get_channel(raw.channel_id)
-    user: discord.member = client.get_user(raw.user_id)
-    if channel.name == 'buckettest':
-        if raw.emoji in emojiDict:
-            # Finds out what 'guild' a user is in and gets the role by ID
-            role = user.guild.get_role(emojiDict[raw.emoji])
-            await user.add_roles(role)
+    user_guild: discord.guild = client.get_guild(raw.guild_id)
+    member = user_guild.get_member(raw.user_id)
+    if channel.name == roles_channel:
+        if raw.emoji.is_custom_emoji():
+            emoji_code = '<:'+raw.emoji.name+':'+str(raw.emoji.id)+'>'
+            role = user_guild.get_role(emojiList[emoji_code])
+            await member.add_roles(role)
+            # await channel.send(f"Gave {member.name} the role: {role.name}")
+        else:
+            role = user_guild.get_role(emojiList[raw.emoji.name])
+            await member.add_roles(role)
+            # TODO Below can be deleted after initial roles are added
     if channel.name == 'roles':
-        print(json.dumps(raw.emoji.name))
+        if raw.emoji.is_custom_emoji():
+            emoji_code = '<:' + raw.emoji.name + ':' + str(raw.emoji.id) + '>'
+            print(emoji_code)
+        else:
+            print(json.dumps(raw.emoji.name))
+
 
 @client.event
 async def on_raw_reaction_remove(raw):
     channel = client.get_channel(raw.channel_id)
-    user = client.get_user(raw.user_id)
-    if channel.name == 'buckettest':
-        if raw.emoji in emojiDict:
-            # Finds out what 'guild' a user is in and gets the role by ID
-            role = user.guild.get_role(emojiDict[raw.emoji])
-            await user.remove_roles(role)
-    if channel.name == 'roles':
-        print("Hey")
-        print(raw.emoji.name.encode('utf-8'))
+    user_guild: discord.guild = client.get_guild(raw.guild_id)
+    member = user_guild.get_member(raw.user_id)
+    if channel.name == roles_channel:
+        if raw.emoji.is_custom_emoji():
+            thingy = '<:'+raw.emoji.name+':'+str(raw.emoji.id)+'>'
+            role = user_guild.get_role(emojiList[thingy])
+            await member.remove_roles(role)
+            # await channel.send(f"Gave {member.name} the role: {role.name}")
+        else:
+            role = user_guild.get_role(emojiList[raw.emoji.name])
+            await member.remove_roles(role)
 
 client.run(token)
