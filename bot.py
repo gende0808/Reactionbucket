67ae61bed80f28f212ad631bb3f29d37fad3eb5b
Gregory <gende@novacollege.nl>
2019-06-24 07:39:05 +0200

    lots of progress

--- bot.py
+++ bot.py
@@ -24,12 +24,14 @@ emojifile = open('db/Emoji.json')
 emojiList = json.load(emojifile)
 emojifile.close()
 mycursor = connection.cursor()
-for records in emojiList:
-    print(records)
-    sql = f"INSERT INTO emoji (message_id, emoji_id, role_id) VALUES (%s, %s, %s)"
-    val = (records[0], records[1], records[2])
-    mycursor.execute(sql, val)
-    connection.commit()
+
+
+def convert_json_to_mariadb():
+    for records in emojiList:
+        sql = "INSERT INTO emoji (message_id, emoji_id, role_id) VALUES (%s, %s, %s)"
+        val = (records[0], records[1], records[2])
+        mycursor.execute(sql, val)
+        connection.commit()
 
 
 @client.event
@@ -53,39 +55,49 @@ async def manage_role(ctx, message_id: discord.Message.id, emoji, role: discord.
 @client.event
 async def on_raw_reaction_add(raw):
     channel = client.get_channel(raw.channel_id)
-    user_guild: discord.guild = client.get_guild(raw.guild_id)
-    member = user_guild.get_member(raw.user_id)
     if channel.name == roles_channel:
-        if raw.emoji.is_custom_emoji():
-            emoji_code = '<:'+raw.emoji.name+':'+str(raw.emoji.id)+'>'
-            role = user_guild.get_role(emojiList[emoji_code])
-            await member.add_roles(role)
-            # await channel.send(f"Gave {member.name} the role: {role.name}")
-        else:
-            role = user_guild.get_role(emojiList[raw.emoji.name])
-            await member.add_roles(role)
-            # TODO Below can be deleted after initial roles are added
-    if channel.name == 'roles':
-        if raw.emoji.is_custom_emoji():
-            emoji_code = '<:' + raw.emoji.name + ':' + str(raw.emoji.id) + '>'
-            print(emoji_code)
-        else:
-            print(json.dumps(raw.emoji.name))
+        role_id = await get_role_from_db(raw)
+        await remove_or_add_roles("add", role_id, raw)
+        # TODO Below can be deleted after initial roles are added
+        # if channel.name == 'roles':
+        # if raw.emoji.is_custom_emoji():
+        #     emoji_code = '<:' + raw.emoji.name + ':' + str(raw.emoji.id) + '>'
+        #     await check_db_for_emoji(emoji_code)
+        # else:
+        #     await check_db_for_emoji(raw.emoji.name)
 
 
 @client.event
 async def on_raw_reaction_remove(raw):
     channel = client.get_channel(raw.channel_id)
+    if channel.name == roles_channel:
+        role_id = await get_role_from_db(raw)
+        await remove_or_add_roles("remove", role_id, raw)
+
+
+async def get_role_from_db(raw: discord.RawReactionActionEvent):
+    if raw.emoji.is_custom_emoji():
+        emoji_code = '<:' + raw.emoji.name + ':' + str(raw.emoji.id) + '>'
+    else:
+        emoji_code = raw.emoji.name
+    sql = "SELECT role_id FROM emoji WHERE message_id = %s AND emoji_id = %s"
+    search_for = (raw.message_id, emoji_code)
+    mycursor.execute(sql, search_for)
+    result = mycursor.fetchone()
+    if result is not None:
+        return result[0]
+    else:
+        return False
+
+
+async def remove_or_add_roles(remove_or_add, role_id, raw: discord.RawReactionActionEvent):
     user_guild: discord.guild = client.get_guild(raw.guild_id)
     member = user_guild.get_member(raw.user_id)
-    if channel.name == roles_channel:
-        if raw.emoji.is_custom_emoji():
-            thingy = '<:'+raw.emoji.name+':'+str(raw.emoji.id)+'>'
-            role = user_guild.get_role(emojiList[thingy])
-            await member.remove_roles(role)
-            # await channel.send(f"Gave {member.name} the role: {role.name}")
-        else:
-            role = user_guild.get_role(emojiList[raw.emoji.name])
+    if role_id is not False:
+        role = user_guild.get_role(role_id)
+        if remove_or_add == "add":
+            await member.add_roles(role)
+        if remove_or_add == "remove":
             await member.remove_roles(role)
 
 client.run(token)
